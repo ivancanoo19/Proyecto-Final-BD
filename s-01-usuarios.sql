d030d9fd81267d567b8536ec76a89d6a17fea525
ivancano <ivan.fernandez@ingenieria.unam.edu>
2024-05-08 15:37:34 -0600

    [ADD] Usuarios y roles creados

---
+++ s-01-usuarios.sql
@@ -0,0 +1,37 @@
+--@Autor(es): Bueno Hernández Héctor Daniel, Fernández Cano Iván Antonio
+--@Fecha creación: 08/05/2024
+--@Descripción: Practica 09 - Ejercicio 1
+
+Prompt Conectando como usuario sys
+connect sys/system as sysdba
+Prompt Limpiando...
+
+declare 
+cursor cur_usuarios is
+    select username from dba_users where username like 'BF_PROY%';
+  cursor cur_roles is
+    select role from dba_roles where role like 'BF_PROY%';
+    
+begin
+  for r in cur_usuarios loop
+  execute immediate 'drop user '||r.username||' cascade';
+  end loop;
+  
+  for r in cur_roles loop
+  execute immediate 'drop role '||r.role;
+  end loop;
+end;
+/
+
+Prompt Creando usuarios otorgando permisos
+create user bf_proy_invitado identified by invitado;
+create user bf_proy_admin identified by admin quota 1024m on users;
+
+create role rol__proy_invitado not identified;
+grant create session to bf_proy_invitado;
+
+create role rol__proy_admin not identified;
+grant create session, create table, create sequence, create procedure, create synonym, create view to bf_proy_admin;
+
+Prompt Listo!
+disconnect
