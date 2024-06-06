73c366b9c020ccdd4639109c558e88f96d25ef30
ivancano <ivan.fernandez@ingenieria.unam.edu>
2024-06-05 18:05:48 -0600

    [MOD] Agregando vistas de CO2 e infracciones

--- s-08-vistas.sql
+++ s-08-vistas.sql
@@ -7,7 +7,7 @@ connect bf_proy_admin/admin
 
 Prompt
 Prompt
-Prompt Creando vistas
+Prompt Creando vista v_placas vehiculo
 create or replace view v_placas_vehiculo(
   vehiculo_id, nombre_modelo, nombre_marca, num_placa, esta_activa, clave
 ) as 
@@ -18,4 +18,28 @@ create or replace view v_placas_vehiculo(
     and v.vehiculo_id=p.vehiculo_id
     and v.modelo_id=mo.modelo_id
     and ma.marca_id=mo.marca_id;
-    
+
+Prompt Creando vista para el promedio de CO2
+create or replace view v_promedio_co2(
+  vehiculo_id, num_serie, modelo, promedio
+) as
+  select vc.vehiculo_id, v.num_serie, mo.nombre as modelo, avg(vc.medida) as promedio
+  from vehiculo_contaminante vc, contaminante c, vehiculo v, modelo mo
+  where c.contaminante_id = vc.contaminante_id
+    and v.vehiculo_id = vc.vehiculo_id
+    and v.modelo_id = mo.modelo_id
+    and c.clave='CO2'
+  group by vc.vehiculo_id, v.num_serie, mo.nombre;
+
+
+Prompt Creando vista que muestra el numero de infracciones
+Prompt de los propietarios
+
+create or replace view v_num_infracciones(
+  propietario_id, nombre, ap_paterno, ap_materno, rfc, num_infracciones
+) as
+  select d.propietario_id, d.nombre, d.ap_paterno, d.ap_materno, d.rfc,
+    count(*) as num_infracciones
+  from s_dueño d
+  left join infraccion i on d.propietario_id=i.propietario_id
+  group by d.propietario_id, d.nombre, d.ap_paterno, d.ap_materno, d.rfc;
