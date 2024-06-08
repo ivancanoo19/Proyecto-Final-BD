a5ce36b8c6e4ae3ef87fea08c792eaa8c2a2b969
Buenoh23 <hectordanielbh@gmail.com>
2024-06-08 08:22:08 -0600

    [ADD] Archivo main para eliminar al usuario en caso qu eexista y crear todos los objetos

---
+++ s-00-main.sql
@@ -0,0 +1,13 @@
+--@Autor(es):       Bueno Hernández Héctor Daniel, Fernández Cano Iván Antonio
+--@Fecha creación:  08/06/2024
+--@Descripción:     Proyecto final - Main
+
+@s-01-usuarios.sql
+@s-02-entidades.sql
+@s-03-tablas-temporales.sql
+@s-04-tablas-externas.sql
+@s-05-secuencias.sql
+@s-06-indices.sql
+@s-07-sinonimos.sql
+@s-08-vistas.sql
+@s-09-carga-inicial.sql
